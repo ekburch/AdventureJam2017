16cd971b0548b9f2c50e9aebbdefec227f27cd67
Ernesto Rojo Jr <gamedev@ernestorojo.com>
2017-05-09 10:34:18 -0400

    Added Fungus and some empty scenes to transition between

---
+++ src/Assets/Editor/ScenesManagerEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(ScenesManager), true)]
+[CanEditMultipleObjects]
+public class ScenesManagerEditor : Editor
+{
+    GUIContent m_mainMenuSceneLabel;
+
+    protected bool m_initialized;
+    protected ScenesManager m_sceneManager;
+
+    public override void OnInspectorGUI()
+    {
+        Init();
+
+        serializedObject.Update();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void Init()
+    {
+        if (m_initialized)
+            return;
+
+        m_initialized = true;
+        m_sceneManager = target as ScenesManager;
+
+        m_mainMenuSceneLabel = new GUIContent("Main Menu Scene", "The scene loaded when the game first start and at game over.");
+    }
+
+    protected void ShowScenes()
+    {
+        var mainMenuObj = GetSceneObject(m_sceneManager.MainMenuScene);
+    }
+
+    protected SceneAsset GetSceneObject(string sceneObjectName)
+    {
+        if (String.IsNullOrEmpty(sceneObjectName))
+            return null;
+
+        foreach( var editorScene in EditorBuildSettings.scenes)
+        {
+            if (editorScene.path.IndexOf(sceneObjectName) != -1)
+                return AssetDatabase.LoadAssetAtPath(editorScene.path, typeof(SceneAsset)) as SceneAsset;
+        }
+    }
+}
---
+++ src/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    [SerializeField] string m_mainMenuScene;
+    [SerializeField] string m_cityScene;
+    [SerializeField] string m_apartmentScene;
+
+    public string MainMenuScene { get { return m_mainMenuScene; } set { m_mainMenuScene = value; } }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
