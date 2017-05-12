3b373aab26fa4676d11fdf6566941753c6d65c01
Ernesto Rojo Jr <gamedev@ernestorojo.com>
2017-05-12 02:13:10 -0400

    Added a super basic main menu. Finally got around to getting `ScenesManager` working and compiling.
    Added some UI and fonts from Kenney.

--- src/Assets/Editor/ScenesManagerEditor.cs
+++ src/Assets/Editor/ScenesManagerEditor.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngineInternal;
 using UnityEditor;
 using System;
+using UnityEngine.Networking;
 
 [CustomEditor(typeof(ScenesManager), true)]
 [CanEditMultipleObjects]
 public class ScenesManagerEditor : Editor
 {
     GUIContent m_mainMenuSceneLabel;
+    GUIContent m_citySceneLabel;
+    GUIContent m_aptSceneLabel;
 
     protected bool m_initialized;
     protected ScenesManager m_sceneManager;
@@ -20,6 +23,7 @@ public class ScenesManagerEditor : Editor
 
         serializedObject.Update();
 
+        ShowScenes();
         serializedObject.ApplyModifiedProperties();
     }
 
@@ -32,23 +36,102 @@ public class ScenesManagerEditor : Editor
         m_sceneManager = target as ScenesManager;
 
         m_mainMenuSceneLabel = new GUIContent("Main Menu Scene", "The scene loaded when the game first start and at game over.");
+        m_citySceneLabel = new GUIContent("City Scene", "The scene when the player is walking towards the friend's house.");
+        m_aptSceneLabel = new GUIContent("Apartment Scene", "The main scene where player visits the friend's house.");
     }
 
-//    protected void ShowScenes()
-//    {
-//        var mainMenuObj = GetSceneObject(m_sceneManager.MainMenuScene);
-//    }
+    protected void ShowScenes()
+    {
+        // Main menu
+        var mainMenuObj = GetSceneObject(m_sceneManager.MainMenuScene);
+        var newMainMenuScene = EditorGUILayout.ObjectField(m_mainMenuSceneLabel, mainMenuObj, typeof(SceneAsset), false);
+        if (newMainMenuScene == null)
+        {
+            var prop = serializedObject.FindProperty("m_mainMenuScene");
+            prop.stringValue = "";
+            EditorUtility.SetDirty(target);
+        }
+        else
+        {
+            if (newMainMenuScene.name != m_sceneManager.MainMenuScene)
+            {
+                var sceneObj = GetSceneObject(newMainMenuScene.name);
+                if (sceneObj == null)
+                    Debug.LogWarning("The scene " + newMainMenuScene.name + " cannot be used. To use this scene add it to the build settings for the project");
+                else
+                {
+                    var prop = serializedObject.FindProperty("m_mainMenuScene");
+                    prop.stringValue = newMainMenuScene.name;
+                    EditorUtility.SetDirty(target);
+                }
+            }
+        }
 
-//    protected SceneAsset GetSceneObject(string sceneObjectName)
-//    {
-//        if (String.IsNullOrEmpty(sceneObjectName))
-//            return null;
-//
-//        foreach( var editorScene in EditorBuildSettings.scenes)
-//        {
-//            if (editorScene.path.IndexOf(sceneObjectName) != -1)
-//                return AssetDatabase.LoadAssetAtPath(editorScene.path, typeof(SceneAsset)) as SceneAsset;
-//
-//        }
-//    }
+        // City
+        var citySceneObj = GetSceneObject(m_sceneManager.CityScene);
+        var newCityScene = EditorGUILayout.ObjectField(m_citySceneLabel, citySceneObj, typeof(SceneAsset), false);
+        if (newCityScene == null)
+        {
+            var prop = serializedObject.FindProperty("m_cityScene");
+            prop.stringValue = "";
+            EditorUtility.SetDirty(target);
+        }
+        else
+        {
+            if (newCityScene.name != m_sceneManager.CityScene)
+            {
+                var sceneObj = GetSceneObject(newCityScene.name);
+                if (sceneObj == null)
+                    Debug.LogWarning("The scene " + newCityScene.name + " cannot be used. To use this scene add it to the build settings for the project");
+                else
+                {
+                    var prop = serializedObject.FindProperty("m_cityScene");
+                    prop.stringValue = newCityScene.name;
+                    EditorUtility.SetDirty(target);
+                }
+            }
+        }
+
+        // Apartment
+        var aptSceneObj = GetSceneObject(m_sceneManager.ApartmentScene);
+        var newAptScene = EditorGUILayout.ObjectField(m_aptSceneLabel, aptSceneObj, typeof(SceneAsset), false);
+        if (newAptScene == null)
+        {
+            var prop = serializedObject.FindProperty("m_apartmentScene");
+            prop.stringValue = "";
+            EditorUtility.SetDirty(target);
+        }
+        else
+        {
+            if (newAptScene.name != m_sceneManager.ApartmentScene)
+            {
+                var sceneObj = GetSceneObject(newAptScene.name);
+                if (sceneObj == null)
+                    Debug.LogWarning("The scene " + newAptScene.name + " cannot be used. To use this scene add it to the build settings for the project");
+                else
+                {
+                    var prop = serializedObject.FindProperty("m_apartmentScene");
+                    prop.stringValue = newAptScene.name;
+                    EditorUtility.SetDirty(target);
+                }
+            }
+        }
+    }
+
+    protected SceneAsset GetSceneObject(string sceneObjectName)
+    {
+        if (String.IsNullOrEmpty(sceneObjectName))
+            return null;
+
+        foreach (var editorScene in EditorBuildSettings.scenes)
+        {
+            if (editorScene.path.IndexOf(sceneObjectName) != -1)
+                return AssetDatabase.LoadAssetAtPath(editorScene.path, typeof(SceneAsset)) as SceneAsset;
+
+        }
+
+        if (LogFilter.logWarn)
+            Debug.LogWarning("Scene [" + sceneObjectName + "] cannot be used. Add this scene to the 'Scenes in the Build' in build settings.");
+        return null;
+    }
 }
--- src/Assets/Scripts/ScenesManager.cs
+++ src/Assets/Scripts/ScenesManager.cs
@@ -11,15 +11,20 @@ public class ScenesManager : MonoBehaviour
     [SerializeField] string m_apartmentScene;
 
     public string MainMenuScene { get { return m_mainMenuScene; } set { m_mainMenuScene = value; } }
+    public string CityScene { get { return m_cityScene; } set { m_cityScene = value; } }
+    public string ApartmentScene { get { return m_apartmentScene; } set { m_apartmentScene = value; } }
 
     // Use this for initialization
     void Start()
     {
+        DontDestroyOnLoad(this);
+
+        SceneManager.LoadScene(MainMenuScene, LoadSceneMode.Additive);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToCityScene()
     {
-
+        SceneManager.UnloadSceneAsync(MainMenuScene);
+        SceneManager.LoadScene(CityScene, LoadSceneMode.Additive);
     }
 }
