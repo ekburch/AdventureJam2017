fdc6022d3d4837710cf18038276a3099e0e981b6
Ethan Burch <ethanburch@MacBook-Pro-61.local>
2017-05-15 23:18:26 -0400

    Rough environment built. Added constraints to CameraFollow. Added colliders to all game objects and altered existing for Player.

--- src/Assets/Scripts/CameraFollow.cs
+++ src/Assets/Scripts/CameraFollow.cs
@@ -7,8 +7,11 @@ public class CameraFollow : MonoBehaviour
 {
 	public Transform target;
 	public float smoothing;
+	public float xMin;
+	public float xMax;
+	public float zMin;
+	public float zMax;
 	Vector3 offset;
-
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,6 +22,9 @@ public class CameraFollow : MonoBehaviour
 	void FixedUpdate () 
 	{
 		Vector3 targetCamPos = target.position + offset;
-		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);	
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.position = new Vector3 (Mathf.Clamp (transform.position.x, xMin, xMax), 
+										 transform.position.y,
+										 (Mathf.Clamp (transform.position.z, zMin, zMax)));
 	}
 }
